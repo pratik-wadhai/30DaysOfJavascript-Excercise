ef164f25602c3085012dac786bbd8f12c28b519e
Pratik Wadhai <81483293+pratik-wadhai@users.noreply.github.com>
2022-08-16 12:21:38 +0530

    Add files via upload

---
+++ Day 1/datatypes.js
@@ -0,0 +1,28 @@
+let name = "pratik";
+let isMarried = false;
+let empty = null;
+let firstName1;
+console.log(typeof name);
+console.log(typeof isMarried);
+console.log(typeof empty);
+console.log(typeof firstName);
+let name2;
+let age;
+let city;
+let isAlive;
+
+let game = "GTA V";
+let location = "mumbai";
+let isDead = false;
+let age2 = 20;
+
+let firstName = "pratik";
+let lastName = "wadhai";
+let maritalStatus = false;
+let country = "India";
+let age3 = 20;
+
+let firstName2 = "pratik",
+  lastName2 = "wadhai",
+  maritalStatus2 = false,
+  age4 = 20;
---
+++ Day 1/variable.js
@@ -0,0 +1,9 @@
+// commenst can make code readable
+// Welcome to 30DaysOfJavaScript
+/* comments can make code readable, 
+    easy to reuse and informative
+*/
+let name = "pratik";
+let isMarried = false;
+let  empty = null;
+let firstName;
