051cf1cafa06b57ec938b78fc463e90cee5a4546
Pratik Wadhai <81483293+pratik-wadhai@users.noreply.github.com>
2022-08-18 16:34:21 +0530

    Create index.js

---
+++ Day 3/index.js
@@ -0,0 +1,54 @@
+let word = "javascript";
+word[0] = "Y";
+console.log(word);
+
+//  primitive data types
+
+let numOne = 3;
+let numTwo = 3;
+
+// console.log(numOne == numTwo); // true
+
+let js = "JavaScript";
+let py = "Python";
+
+// console.log(js == py); //false
+
+let lightOn = true;
+let lightOff = false;
+
+// console.log(lightOn == lightOff); // false
+
+// non-primitive data types
+
+let nums = [1, 2, 3];
+let numbers = [1, 2, 3];
+// nums[0] = 20;
+// console.log(nums == numbers);
+
+let userOne = {
+  name: "pratik",
+  age: 20,
+  city: "mumbai",
+};
+let userTwo = {
+  name: "pratik",
+  age: 20,
+  city: "mumbai",
+};
+
+// console.log(userOne == userTwo);
+
+let nums2 = [1, 2, 3];
+let numbers2 = nums2;
+// console.log(nums2 == numbers2);
+
+let userOne2 = {
+  name: "Asabeneh",
+  role: "teaching",
+  country: "Finland",
+};
+
+let userTwo2 = userOne2;
+// console.log(userOne2 == userTwo2);
+
